ce1f955b68ddc0ebf79048103528abf6e32fb90f
Your Name <you@example.com>
2018-06-26 11:07:45 -0300

    improvements

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# python -m pip install pandas
-# python -m pip install numpy
-# python -m pip install sklearn
-# python -m pip install scipy
+# python -m pip install scipy sklearn
 # python -m pip install imblearn
 # python -m pip install --upgrade pip
 # http://contrib.scikit-learn.org/imbalanced-learn/stable/auto_examples/applications/plot_topic_classication.html
 
-# import pandas as pd
-# import numpy as np
 import csv
 import sys
-
 import collections
 
 from sklearn.naive_bayes import BernoulliNB
@@ -51,6 +45,7 @@ def makePipelineBernoulliNB(X_train, Y_train, X_test, Y_test, binarize):
     print('binarize', binarize, accuracy_score(Y_test, y_pred))
     print(classification_report_imbalanced(Y_test, y_pred))
 
+
 ########################################################################
 
 def makePipelineImpBernoulliNB(X_train, Y_train, X_test, Y_test, binarize):
@@ -63,6 +58,7 @@ def makePipelineImpBernoulliNB(X_train, Y_train, X_test, Y_test, binarize):
 
     print('binarize', binarize, accuracy_score(Y_test, y_pred))
     print(classification_report_imbalanced(Y_test, y_pred))
+
 
 ########################################################################
 
@@ -77,17 +73,20 @@ def makePipelineGaussianNB(X_train, Y_train, X_test, Y_test):
     print(accuracy_score(Y_test, y_pred))
     print(classification_report_imbalanced(Y_test, y_pred))
 
+
 ########################################################################
 
 def makePipelineImpGaussianNB(X_train, Y_train, X_test, Y_test):
     pipe = make_pipeline_imb(TfidfVectorizer(),
                              RandomUnderSampler(),
                              GaussianNB())
+
     pipe.fit(X_train, Y_train)
     y_pred = pipe.predict(X_test)
 
     print(accuracy_score(Y_test, y_pred))
     print(classification_report_imbalanced(Y_test, y_pred))
+
 
 ########################################################################
 
@@ -102,6 +101,7 @@ def makePipelineMultinomialNB(X_train, Y_train, X_test, Y_test):
     print(accuracy_score(Y_test, y_pred))
     print(classification_report_imbalanced(Y_test, y_pred))
 
+
 ########################################################################
 
 def makePipelineImpMultinomialNB(X_train, Y_train, X_test, Y_test):
@@ -114,13 +114,17 @@ def makePipelineImpMultinomialNB(X_train, Y_train, X_test, Y_test):
     print(accuracy_score(Y_test, y_pred))
     print(classification_report_imbalanced(Y_test, y_pred))
 
+
 ########################################################################
 
 # Caminho do arquivo
 filename = "dataset/train.tsv"
-
-reader = csv.reader(open(filename, "r"), delimiter="\t")
-dataset = list(reader)
+try:
+    reader = csv.reader(open(filename, "r", encoding='UTF8'), delimiter="\t")
+except:
+    reader = csv.reader(open(filename, "r"), delimiter="\t")
+finally:
+    dataset = list(reader)
 
 trainningSetSize = int(len(dataset) * 0.33)
 
@@ -139,43 +143,50 @@ for data in dataset:
 
     trainningSetSize = trainningSetSize - 1
 
-print('Training class distributions summary: {}'.format(collections.Counter(Y_train)))
+print('Training class distributions summary: {}'.format(
+    collections.Counter(Y_train)))
 print('Test class distributions summary: {}'.format(collections.Counter(Y_test)))
 
-try:
-    print('Pipeline Bernoulli')
-    for x in range(1, 10):
-        makePipelineBernoulliNB(X_train, Y_train, X_test, Y_test, x / 10.0)
-except TypeError:
-    print(TypeError.message)
+# try:
+print('###############################################')
+print('Pipeline Bernoulli')
+for x in range(1, 10):
+    makePipelineBernoulliNB(X_train, Y_train, X_test, Y_test, x / 10.0)
+# except TypeError:
+    # print(TypeError)
 
-try:
-    print('Pipeline Imp Bernoulli')
-    for x in range(1, 10):
-        makePipelineImpBernoulliNB(X_train, Y_train, X_test, Y_test, x / 10.0)
-except TypeError:
-    print(TypeError.message)
+# try:
+print('###############################################')
+print('Pipeline Imp Bernoulli')
+for x in range(1, 10):
+    makePipelineImpBernoulliNB(X_train, Y_train, X_test, Y_test, x / 10.0)
+# except TypeError:
+    # print(TypeError)
 
-try:
-    print('Pipeline Multinomial')
-    makePipelineMultinomialNB(X_train, Y_train, X_test, Y_test)
-except TypeError:
-    print(TypeError.message)
+# try:
+print('###############################################')
+print('Pipeline Multinomial')
+makePipelineMultinomialNB(X_train, Y_train, X_test, Y_test)
+# except TypeError:
+    # print(TypeError)
 
-try:
-    print('Pipeline Imp Multinomial')
-    makePipelineImpMultinomialNB(X_train, Y_train, X_test, Y_test)
-except IndexError:
-    print(TypeError.message)
+# try:
+print('###############################################')
+print('Pipeline Imp Multinomial')
+makePipelineImpMultinomialNB(X_train, Y_train, X_test, Y_test)
+# except TypeError:
+    # print(TypeError)
 
-try: 
-	print('Pipeline Gaussian')
-	makePipelineGaussianNB( X_train, Y_train, X_test, Y_test )
-except TypeError:
-	print(TypeError.message)
+# try:
+print('###############################################')
+print('Pipeline Gaussian')
+makePipelineGaussianNB(X_train, Y_train, X_test, Y_test)
+# except TypeError:
+    # print(TypeError)
 
-try: 
-	print('Pipeline Imp Gaussian')
-	makePipelineImpGaussianNB( X_train, Y_train, X_test, Y_test )
-except TypeError:
-	print(TypeError.message)
+# try:
+print('###############################################')
+print('Pipeline Imp Gaussian')
+makePipelineImpGaussianNB(X_train, Y_train, X_test, Y_test)
+# except TypeError:
+    # print(TypeError)
